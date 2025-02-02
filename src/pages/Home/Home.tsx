789bec780523c3ab6f739e3400ebb9af55ddadc6
lbeneston <benestonlucas@hotmail.fr>
2025-02-02 21:51:53 +0100

    try github actions

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { PartenairesCarrousel } from '../../components/partenairesCarrousel/PartenairesCarrousel'
 import { Services } from '../../components/services/Services'
 import { HomeComponent } from './Styles'
