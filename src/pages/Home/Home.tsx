a2c58474fbd0e4b65ff9f320e05b34588e3aed5a
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 20:30:54 +0200

    Replace Agency component with Services component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom'
 import { HomeSection } from '../../components/home/Home'
-import { Agency } from '../../components/agency/Agency'
+import { Services } from '../../components/services/Services'
 
 export const Home = () => {
   return (
     <div>
       <h1>HOME</h1>
       <HomeSection />
-      <Agency />
+      <Services />
       <Link to="/agence">
         Aller à la page 2
       </Link>
