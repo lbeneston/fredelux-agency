68b0d7d7f7e51835541f535bdddfd3f85c15cfcc
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 20:56:09 +0200

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
 import { Agency } from './pages/Agency/Agency'
+import { SoundDesign } from './pages/SoundDesign/SoundDesign'
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/agence" element={<Agency />} />
+      <Route path="/sound-design" element={<SoundDesign />} />
     </Routes>
   )
 }
