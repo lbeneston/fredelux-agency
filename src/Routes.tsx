8a908cba08fce049e45140a106305fe6e9eedffb
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 20:52:29 +0200

    Remove unused Link import from Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
 import { Agency } from './pages/Agency/Agency'
 
