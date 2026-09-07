a38554f67cc533eea4b0bfe70f74528ff9433fba
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 20:30:10 +0200

    Add Agency component to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
 import { HomeSection } from '../../components/home/Home'
+import { Agency } from '../../components/agency/Agency'
 
 export const Home = () => {
   return (
     <div>
       <h1>HOME</h1>
       <HomeSection />
+      <Agency />
       <Link to="/agence">
         Aller à la page 2
       </Link>
