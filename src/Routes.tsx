5be7776c132a46d153b7d37cd370a73dc496544b
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 19:42:59 +0200

    Clean up imports in Routes.tsx
    Removed unused imports from Routes.tsx.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
-import { NoMatch } from './pages/NoMatch/NoMatch'
-import { SoundDesign } from './pages/SoundDesign/SoundDesign'
-import Booking from './pages/Booking/Booking'
-import { DjCard } from './pages/DJCard/DJCard'
-import { Contact } from './pages/Contact/Contact'
 import { Agency } from './pages/Agency/Agency'
-import { useEffect } from 'react'
 
 export const AppRoutes = () => {
   return (
