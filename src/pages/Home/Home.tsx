182b6c86bed7cd63d23ef59901f3ace493d2fab8
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 20:26:49 +0200

    Add HomeSection component to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
+import { HomeSection } from '../../components/home/Home'
 
 export const Home = () => {
   return (
     <div>
       <h1>HOME</h1>
-
+      <HomeSection />
       <Link to="/agence">
         Aller à la page 2
       </Link>
