7ce986236f55455f8c8c6f2a8f787183b99aa613
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 20:18:16 +0200

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { useScrollToTop } from '../../helpers/useScrollToTop'
 import { Link } from 'react-router-dom'
 
-
 export const Home = () => {
-  useScrollToTop()
-
   return (
-    <React.Fragment>
-      <Link to="/agence">Aller à la page 2</Link>
-    </React.Fragment >
+    <div>
+      <h1>HOME</h1>
+
+      <Link to="/agence">
+        Aller à la page 2
+      </Link>
+    </div>
   )
 }
