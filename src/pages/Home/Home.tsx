d917d8e93d0f5676e983bea6a4dd684afd354b6c
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 21:22:10 +0200

    Refactor Home component structure and imports
    Refactored Home component to use React.Fragment and updated imports.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,17 @@
 import { Link } from 'react-router-dom'
-import { HomeSection } from '../../components/home/Home'
-import { Services } from '../../components/services/Services'
 import { PartenairesCarrousel } from '../../components/partenairesCarrousel/PartenairesCarrousel'
+import { Services } from '../../components/services/Services'
 import { Agency } from '../Agency/Agency'
+import { Contact } from '../Contact/Contact'
+import { HomeSection } from '../../components/home/Home'
 
 export const Home = () => {
   return (
-    <div>
-      <h1>HOME</h1>
+    <React.Fragment>
       <HomeSection />
-      <Link to="/agence">Aller à la page "agence"</Link>
-      <Link to="/sound-design">Aller à la page "sound-design"</Link>
-      <Link to="/booking">Aller à la page "booking"</Link>
-      <Link to="/booking/:url_name">Aller à la page "booking/:url_name"</Link>
-      <Link to="/contact">Aller à la page "contact"</Link>
       <Agency />
       <Services />
+      <Contact />
       <PartenairesCarrousel />
     </div>
   )
