e5478b2400e08ce13abb521bc1af411fa7c79bdd
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 19:20:40 +0200

    Refactor routing to use AppRoutes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
-
-const HomeTest = () => (
-  <div>
-    <h1>Accueil TEST</h1>
-    <Link to="/agence">Aller à Agence</Link>
-  </div>
-)
-
-const AgencyTest = () => (
-  <div>
-    <h1>Agence TEST</h1>
-    <Link to="/">Retour accueil</Link>
-  </div>
-)
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './Routes'
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeTest />} />
-        <Route path="/agence" element={<AgencyTest />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   )
 }
