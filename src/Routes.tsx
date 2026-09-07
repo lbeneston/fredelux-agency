7ad579fd7e9e9a79fbb6f3adecc7f7faaa8e2aaa
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 21:12:41 +0200

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
-import { Agency } from './pages/Agency/Agency'
+import { NoMatch } from './pages/NoMatch/NoMatch'
 import { SoundDesign } from './pages/SoundDesign/SoundDesign'
+import Booking from './pages/Booking/Booking'
+import { DjCard } from './pages/DJCard/DJCard'
+import { Contact } from './pages/Contact/Contact'
+import { Agency } from './pages/Agency/Agency'
 
 export const AppRoutes = () => {
   return (
@@ -9,6 +13,10 @@ export const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/agence" element={<Agency />} />
       <Route path="/sound-design" element={<SoundDesign />} />
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/booking/:url_name" element={<DjCard />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NoMatch />} />
     </Routes>
   )
 }
