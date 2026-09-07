26702dbec1f50efe97a2bc0a27996feb29635f24
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 21:30:27 +0200

    Remove scroll to top effect in App component
    Removed scroll to top effect on route change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Footer } from './components/footer/Footer'
 import { Nav } from './components/nav/Nav'
@@ -7,12 +6,6 @@ import { AppRoutes } from './Routes'
 import { Fonts } from './styles/Fonts'
 
 export const App = () => {
-  const { pathname } = useLocation() 
-    
-  useEffect(() => { 
-    window.scrollTo({ top: 0, left: 0, behavior: 'auto', }) 
-  }, [pathname])
-  
   return (
     <React.Fragment>
       <GlobalStyle />
