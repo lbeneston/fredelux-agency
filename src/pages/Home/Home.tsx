40e92ddc9ed757e2a1ae8ca700776133486b41f3
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 21:25:21 +0200

    Add React import to Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { Link } from 'react-router-dom'
 import { PartenairesCarrousel } from '../../components/partenairesCarrousel/PartenairesCarrousel'
 import { Services } from '../../components/services/Services'
