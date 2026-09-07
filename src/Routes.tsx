84fe539151559f62a38ee09df9f6f6e51b4da4c7
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 19:26:08 +0200

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,27 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home/Home'
-import { NoMatch } from './pages/NoMatch/NoMatch'
-import { SoundDesign } from './pages/SoundDesign/SoundDesign'
-import Booking from './pages/Booking/Booking'
-import { DjCard } from './pages/DJCard/DJCard'
-import { Contact } from './pages/Contact/Contact'
-import { Agency } from './pages/Agency/Agency'
-import { useEffect } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+
+const TestHome = () => (
+  <div>
+    <h1>HOME TEST</h1>
+    <Link to="/agence">Agence</Link>
+  </div>
+)
+
+const TestAgency = () => (
+  <div>
+    <h1>AGENCE TEST</h1>
+    <Link to="/">Accueil</Link>
+  </div>
+)
 
 export const AppRoutes = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/agence" element={<Agency />} />
-      <Route path="/sound-design" element={<SoundDesign />} />
-      <Route path="/booking" element={<Booking />} />
-      <Route path="/booking/:url_name" element={<DjCard />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NoMatch />} />
+      <Route path="/" element={<TestHome />} />
+      <Route path="/agence" element={<TestAgency />} />
     </Routes>
   )
 }
