ee2d73a5ad4bf6c9272c1ed14242f50543b07838
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 19:29:31 +0200

    Replace test routes with Home component
    Removed TestHome and TestAgency components and replaced them with Home component in AppRoutes.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,24 +1,10 @@
-import { Routes, Route, Link } from 'react-router-dom'
-
-const TestHome = () => (
-  <div>
-    <h1>HOME TEST</h1>
-    <Link to="/agence">Agence</Link>
-  </div>
-)
-
-const TestAgency = () => (
-  <div>
-    <h1>AGENCE TEST</h1>
-    <Link to="/">Accueil</Link>
-  </div>
-)
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home/Home'
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<TestHome />} />
-      <Route path="/agence" element={<TestAgency />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   )
 }
