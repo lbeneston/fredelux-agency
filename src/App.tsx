ab3e0472783273caa614b32d1845adf6b0c4b6b5
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 21:13:30 +0200

    Refactor App component to include Nav and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Footer } from './components/footer/Footer'
+import { Nav } from './components/nav/Nav'
+import { GlobalStyle } from './styles/GlobalStyle'
 import { AppRoutes } from './Routes'
+import { Fonts } from './styles/Fonts'
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <React.Fragment>
+      <GlobalStyle />
+      <Fonts />
+      <BrowserRouter useTransitions={false}>
+        <Nav />
+        <AppRoutes />
+      </BrowserRouter>
+      <Footer />
+    </React.Fragment>
   )
 }
