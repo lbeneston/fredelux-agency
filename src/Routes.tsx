64680ebbe8fa46d96543bc127c97812e131e2dfa
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 20:03:20 +0200

    Replace Home and Agency with PageOne and PageTwo

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home/Home'
-import { Agency } from './pages/Agency/Agency'
+import { Routes, Route, Link } from 'react-router-dom'
+
+const PageOne = () => (
+  <div>
+    <h1>PAGE 1</h1>
+    <Link to="/agence">Aller à la page 2</Link>
+  </div>
+)
+
+const PageTwo = () => (
+  <div>
+    <h1>PAGE 2</h1>
+    <Link to="/">Retour à la page 1</Link>
+  </div>
+)
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/agence" element={<Agency />} />
+      <Route path="/" element={<PageOne />} />
+      <Route path="/agence" element={<PageTwo />} />
     </Routes>
   )
 }
