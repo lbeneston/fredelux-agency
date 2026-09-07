ed4fd745f5da75e4a0180f5f095ef1a8cd310403
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 19:56:10 +0200

    Refactor section header illustration styles

--- src/components/section/Styles.ts
+++ src/components/section/Styles.ts
@@ -63,32 +63,27 @@ export const SectionComponent = styled.section<{ id?: string }>`
       }
     }
 
-  & .section_header_illustration {
-    height: 100%;
-    width: 75%;
-    left: 20%;
-    position: absolute;
-  
-    & picture {
+    & .section_header_illustration {
       height: 100%;
-      width: 100%;
-      display: block;
-  
+      width: 75%;
+      left: 20%;
+      position: absolute;
+
       @media (min-width: 768px) {
         display: flex;
         align-items: center;
       }
-    }
-  
-    & picture img {
-      height: 100%;
-      width: 100%;
-      object-fit: cover;
-      filter: grayscale(100%);
-      background-color: ${colors.grey};
-  
-      @media (min-width: 768px) {
-        height: 75%;
+
+      & img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        filter: grayscale(100%);
+        background-color: ${colors.grey};
+
+        @media (min-width: 768px) {
+          height: 75%;
+        }
       }
     }
   }
