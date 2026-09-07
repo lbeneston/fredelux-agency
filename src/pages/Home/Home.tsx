4e8546e15d089e0d260eddacbfdde50501966492
lbeneston <benestonlucas@hotmail.fr>
2026-09-07 21:33:29 +0200

    Remove unused Link import from Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom'
 import { PartenairesCarrousel } from '../../components/partenairesCarrousel/PartenairesCarrousel'
 import { Services } from '../../components/services/Services'
 import { Agency } from '../Agency/Agency'
